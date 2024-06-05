128a771ae16ab24faa4cc2b793f3570b83690951
Aryan C <aryanc1027@gmail.com>
2024-06-04 23:31:01 -0400

    Added ios functionality

--- main.js
+++ main.js
@@ -7,30 +7,52 @@ const btnNo = document.querySelector(".btn-no");
 function getRandomNumber(min, max) {
   // Calculate the random number between min and max (inclusive)
   const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-
   return randomNumber;
 }
 
+// Function to detect if the device is iOS
+function isIOS() {
+  return /iPad|iPhone|iPod/.test(navigator.userAgent);
+}
+
+// Function to shrink the button
+function shrinkButton(button) {
+  const currentWidth = button.offsetWidth;
+  const currentHeight = button.offsetHeight;
+  const shrinkFactor = 0.9; // Shrink by 10%
+
+  button.style.width = `${currentWidth * shrinkFactor}px`;
+  button.style.height = `${currentHeight * shrinkFactor}px`;
+}
+
 btnNo.addEventListener("mouseover", (event) => {
-  const containerHeight = container.getBoundingClientRect().height;
-  const containerWidth = container.getBoundingClientRect().width;
-  const btnHeight = btnNo.getBoundingClientRect().height;
-  const btnWidth = btnNo.getBoundingClientRect().width;
-  const btnTop = btnNo.getBoundingClientRect().top;
-  const btnLeft = btnNo.getBoundingClientRect().left;
+  if (!isIOS()) {
+    const containerHeight = container.getBoundingClientRect().height;
+    const containerWidth = container.getBoundingClientRect().width;
+    const btnHeight = btnNo.getBoundingClientRect().height;
+    const btnWidth = btnNo.getBoundingClientRect().width;
+    const btnTop = btnNo.getBoundingClientRect().top;
+    const btnLeft = btnNo.getBoundingClientRect().left;
 
-  let newTop = btnTop;
-  let newLeft = btnLeft;
-  while (Math.abs(newTop - btnTop) < containerHeight / 3) {
-    newTop = getRandomNumber(0, containerHeight - btnHeight);
+    let newTop = btnTop;
+    let newLeft = btnLeft;
+    while (Math.abs(newTop - btnTop) < containerHeight / 3) {
+      newTop = getRandomNumber(0, containerHeight - btnHeight);
+    }
+
+    while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
+      newLeft = getRandomNumber(0, containerWidth - btnWidth);
+    }
+
+    btnNo.style.top = Math.floor(newTop) + "px";
+    btnNo.style.left = Math.floor(newLeft) + "px";
   }
+});
 
-  while (Math.abs(newLeft - btnLeft) < containerWidth / 3) {
-    newLeft = getRandomNumber(0, containerWidth - btnWidth);
+btnNo.addEventListener("click", (e) => {
+  if (isIOS()) {
+    shrinkButton(btnNo);
   }
-
-  btnNo.style.top = Math.floor(newTop) + "px";
-  btnNo.style.left = Math.floor(newLeft) + "px";
 });
 
 btnYes.addEventListener("click", (e) => {
